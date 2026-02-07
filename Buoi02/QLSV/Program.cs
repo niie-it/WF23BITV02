6e3edf09a110fe53f9eaee655d2d0f6900dd19a9
HIENLTH <hyhien@gmail.com>
2026-02-07 09:27:59 +0700

    Demo QLSV

---
+++ Buoi02/QLSV/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLSV
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.UTF8;
+
+            List<Student> students = new List<Student>();
+            int SoLuong;
+            while (true)
+            {
+                Console.Write("Nhập số lượng sv: ");
+                if (int.TryParse(Console.ReadLine(), out SoLuong) && SoLuong > 0)
+                {
+                    break;
+                }
+                Console.WriteLine("Sai số lượng. Vui lòng nhập lại");
+            }
+
+            //Nhập liệu thông tin sv
+            for (int i = 0; i < SoLuong; i++)
+            {
+                //Nhập liệu đúng
+                Console.Write("Họ tên: ");
+                string HoTen = Console.ReadLine().Trim();
+                Console.Write("Chuyên ngành: ");
+                string ChuyenNganh = Console.ReadLine().Trim();
+                int mssv;
+                while (true)
+                {
+                    Console.Write("Mã số sv: ");
+                    if (int.TryParse(Console.ReadLine(), out mssv) && mssv > 0)
+                    {
+                        break;
+                    }
+                    Console.WriteLine("Sai MSSV. Vui lòng nhập lại");
+                }
+                double gpa;
+                while(true)
+                {
+                    Console.Write("Điểm (GPA) [0-4]: ");
+                    if (double.TryParse(Console.ReadLine(), out gpa) && gpa >= 0 && gpa <= 4)
+                    {
+                        break;
+                    }
+                    Console.WriteLine("Sai GPA. Vui lòng nhập lại");
+                }
+
+                students.Add(new Student(HoTen, ChuyenNganh, gpa, mssv));
+
+                //In ds hiện có
+                Console.WriteLine("\n\n=======Danh sách SV hiện có=======\n");
+                foreach (var sv in students)
+                {
+                    Console.WriteLine(sv);
+                }
+            }
+        }
+    }
+}
---
+++ Buoi02/QLSV/Student.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLSV
+{
+    public class Student
+    {
+        public string FullName { get; set; }
+        public string Major { get; set; }
+        public double Gpa
+        {
+            get => _gpa;
+            set
+            {
+                if (value >= 0 && value <= 4) { _gpa = value; }
+                else
+                {
+                    throw new Exception("GPA must in [0.0 - 4.0]");
+                }
+            }
+        }
+        public int Id
+        {
+            get => _id; // get{ return _id;}
+            set
+            {
+                if (value > 0) { _id = value; }
+                else
+                {
+                    throw new Exception("Id must > 0");
+                }
+            }
+        }
+
+        private int _id;
+        private double _gpa;
+
+        public Student() { }
+        public Student(string fullName, string major, double gpa, int id)
+        {
+            FullName = fullName;
+            Major = major;
+            Gpa = gpa;
+            this.Id = id;
+        }
+
+        public override string ToString()
+        {
+            return $"{Id}, {FullName}, {Major}, GPA: {Gpa}";
+        }
+    }
+}
