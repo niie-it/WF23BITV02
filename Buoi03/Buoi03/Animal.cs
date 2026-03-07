a86cb26f3d3c9ae60f87c58301027f565ae2f702
HIENLTH <hyhien@gmail.com>
2026-03-07 08:08:33 +0700

    Demo ke thua

---
+++ Buoi03/Buoi03/Animal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buoi03
+{
+    public class Animal
+    {
+        protected void Sleep() => Console.WriteLine("Animal is sleeping.");
+        public void Eat()
+        {
+            Console.WriteLine("Animal is eating.");
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public void XYZ()
+        {
+            Sleep();
+        }
+        public void Bark()
+        {
+            Console.WriteLine("Dog is barking.");
+        }
+    }
+}
---
+++ Buoi03/Buoi03/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buoi03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = new Dog();
+            x.Eat();
+            //x.Sleep();
+            x.XYZ();
+            x.Bark();
+        }
+    }
+}
