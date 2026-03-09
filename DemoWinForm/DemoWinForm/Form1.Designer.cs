905895d87fcbc949e5502426081d88d030a45ce8
HIENLTH <hyhien@gmail.com>
2026-03-09 12:23:12 +0700

    demo say hello

---
+++ DemoWinForm/DemoWinForm/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace DemoWinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtHoTen = new System.Windows.Forms.TextBox();
+            this.btnChao = new System.Windows.Forms.Button();
+            this.txtKetQua = new System.Windows.Forms.TextBox();
+            this.lblThongBao = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 9.900001F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(99, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(148, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Họ và tên";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtHoTen
+            // 
+            this.txtHoTen.Font = new System.Drawing.Font("Times New Roman", 9.900001F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtHoTen.Location = new System.Drawing.Point(263, 51);
+            this.txtHoTen.Name = "txtHoTen";
+            this.txtHoTen.Size = new System.Drawing.Size(302, 45);
+            this.txtHoTen.TabIndex = 1;
+            this.txtHoTen.TextChanged += new System.EventHandler(this.txtHoTen_TextChanged);
+            this.txtHoTen.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtHoTen_KeyUp);
+            // 
+            // btnChao
+            // 
+            this.btnChao.Font = new System.Drawing.Font("Times New Roman", 9.900001F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChao.Location = new System.Drawing.Point(630, 37);
+            this.btnChao.Name = "btnChao";
+            this.btnChao.Size = new System.Drawing.Size(145, 64);
+            this.btnChao.TabIndex = 2;
+            this.btnChao.Text = "Chào";
+            this.btnChao.UseVisualStyleBackColor = true;
+            this.btnChao.Click += new System.EventHandler(this.btnChao_Click);
+            // 
+            // txtKetQua
+            // 
+            this.txtKetQua.Font = new System.Drawing.Font("Times New Roman", 9.900001F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtKetQua.Location = new System.Drawing.Point(105, 220);
+            this.txtKetQua.Name = "txtKetQua";
+            this.txtKetQua.Size = new System.Drawing.Size(670, 45);
+            this.txtKetQua.TabIndex = 4;
+            // 
+            // lblThongBao
+            // 
+            this.lblThongBao.AutoSize = true;
+            this.lblThongBao.Font = new System.Drawing.Font("Times New Roman", 9.900001F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblThongBao.Location = new System.Drawing.Point(257, 121);
+            this.lblThongBao.Name = "lblThongBao";
+            this.lblThongBao.Size = new System.Drawing.Size(98, 39);
+            this.lblThongBao.TabIndex = 5;
+            this.lblThongBao.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1159, 788);
+            this.Controls.Add(this.lblThongBao);
+            this.Controls.Add(this.txtKetQua);
+            this.Controls.Add(this.btnChao);
+            this.Controls.Add(this.txtHoTen);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtHoTen;
+        private System.Windows.Forms.Button btnChao;
+        private System.Windows.Forms.TextBox txtKetQua;
+        private System.Windows.Forms.Label lblThongBao;
+    }
+}
+
---
+++ DemoWinForm/DemoWinForm/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DemoWinForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnChao_Click(object sender, EventArgs e)
+        {
+            var chuoi = $"Xin chào {txtHoTen.Text}";
+            txtKetQua.Text = chuoi;
+        }
+
+        private void txtHoTen_KeyUp(object sender, KeyEventArgs e)
+        {
+            lblThongBao.Text = txtHoTen.Text;
+        }
+        private void txtHoTen_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
