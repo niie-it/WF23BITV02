57c15933bc76d537923e68ce72f4b58111a5a16b
HIENLTH <hyhien@gmail.com>
2026-04-18 09:29:51 +0700

    In hoa don

---
+++ Lab10QLBanHang/Lab10QLBanHang/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Lab10QLBanHang
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CboHoaDon = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BtnXuatHd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // CboHoaDon
+            // 
+            this.CboHoaDon.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CboHoaDon.FormattingEnabled = true;
+            this.CboHoaDon.Location = new System.Drawing.Point(215, 55);
+            this.CboHoaDon.Name = "CboHoaDon";
+            this.CboHoaDon.Size = new System.Drawing.Size(754, 53);
+            this.CboHoaDon.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(27, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(173, 45);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Hóa đơn";
+            // 
+            // BtnXuatHd
+            // 
+            this.BtnXuatHd.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnXuatHd.Location = new System.Drawing.Point(1121, 55);
+            this.BtnXuatHd.Name = "BtnXuatHd";
+            this.BtnXuatHd.Size = new System.Drawing.Size(387, 77);
+            this.BtnXuatHd.TabIndex = 2;
+            this.BtnXuatHd.Text = "Xuất HĐ (Excel)";
+            this.BtnXuatHd.UseVisualStyleBackColor = true;
+            this.BtnXuatHd.Click += new System.EventHandler(this.BtnXuatHd_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1588, 450);
+            this.Controls.Add(this.BtnXuatHd);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CboHoaDon);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox CboHoaDon;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button BtnXuatHd;
+    }
+}
+
---
+++ Lab10QLBanHang/Lab10QLBanHang/Form1.cs
@@ -0,0 +1,82 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab10QLBanHang
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            //đổ data vào combo box
+            var sql = "select  hd.MaHd, concat(hd.MaHd, ' - ', kh.TenCty, ' - ', hd.NgayLapHD) as TTHD from HoaDon hd JOIN KhachHang kh ON hd.MaKh = kh.MaKh order by hd.NgayLapHD DESC";
+            var dtHoaDon = DataProvider.TruyVan_LayDuLieu(sql);
+            CboHoaDon.DisplayMember = "TTHD";
+            CboHoaDon.ValueMember = "MaHd";
+            CboHoaDon.DataSource = dtHoaDon;
+        }
+
+        private void BtnXuatHd_Click(object sender, EventArgs e)
+        {
+            var maHd = CboHoaDon.SelectedValue.ToString();
+            // MessageBox.Show("Xuất hóa đơn có mã: " + maHd);
+            var fullPath = Path.Combine(Application.StartupPath, "Hoa-don-ban-hang-template.xlsx"); //giả sử file template Hoa-don-ban-hang-template.xlsx đặt cùng chỗ chương trình chạy
+
+            FileInfo path = new FileInfo(fullPath);
+            if (path.Exists)
+            {
+                ExcelPackage.License.SetNonCommercialPersonal("My Name");
+                using (ExcelPackage p = new ExcelPackage(path))
+                {
+                    ExcelWorksheet ws = p.Workbook.Worksheets["Sheet1"];
+                    // Lấy thông tin hóa đơn
+                    var sql = $"select  hd.MaHd, kh.TenCty, kh.DiaChi from HoaDon hd JOIN KhachHang kh ON hd.MaKh = kh.MaKh WHERE hd.MaHd = '{maHd}'";
+                    var ttHoaDon = DataProvider.TruyVan_LayDuLieu(sql);
+
+                    ws.Cells["A4"].Value = $"Tên khách hàng: {ttHoaDon.Rows[0]["TenCty"]}";
+                    ws.Cells["A5"].Value = $"Địa chỉ: {ttHoaDon.Rows[0]["DiaChi"]}";
+
+                    // Lấy thông tin chi tiết hóa đơn
+                    var sqlCTDH = $"SELECT ct.MaHd, ct.SoLuong, sp.TenSp, sp.DonGia, ct.SoLuong * sp.DonGia as ThanhTien FROM ChiTietHoaDon ct JOIN SanPham sp ON ct.MaSp = sp.MaSp WHERE ct.MaHd = '{maHd}'";
+                    var invData = DataProvider.TruyVan_LayDuLieu(sqlCTDH);
+                    int soDong = invData.Rows.Count;
+                    //chèn số dòng đúng với số lượng sản phẩm mua
+                    ws.InsertRow(9, soDong - 1);
+
+                    //Set số thứ tự
+                    ws.Cells[$"A8:A{8 + soDong - 1}"].FillNumber(x => x.StartValue = 1);
+                    ws.Cells[$"C{8 + soDong}"].Formula = $"=SUM(C8:C{8 + soDong - 1})";
+                    ws.Cells[$"E{8 + soDong}"].Formula = $"=SUM(E8:E{8 + soDong - 1})";
+                    var i = 0;
+                    foreach (DataRow dr in invData.Rows)
+                    {
+                        ws.Cells[8 + i, 1].Value = (i + 1).ToString();
+                        ws.Cells[8 + i, 2].Value = dr["TenSp"].ToString();
+                        ws.Cells[8 + i, 3].Value = dr["SoLuong"].ToString();
+                        ws.Cells[8 + i, 4].Value = dr["DonGia"].ToString();
+                        ws.Cells[8 + i, 5].Value = dr["ThanhTien"].ToString();
+                        i++;
+                    }
+
+                    p.SaveAs($"INV{maHd}.xlsx"); // có thể đổi tên file
+
+                }
+
+            }
+        }
+    }
+}
